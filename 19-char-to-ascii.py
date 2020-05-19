6e041b4c4d2fdf207e98d4714260f1043229c778
selimmh <selim.mht@gmail.com>
2020-05-19 23:14:49 +0300

    initial commit

---
+++ 19-char-to-ascii.py
@@ -0,0 +1,5 @@
+# Bu programma 'character'-i 'ASCII'-e öwürýär
+
+char = input("Charachter giriz: ")
+print(char,"-niň ASCII bahasy: ")
+print(ord(char))
---
+++ 20-ascii-to-char.py
@@ -0,0 +1,5 @@
+# Bu programma ASCII bahany 'character'-e öwürýär
+
+asc = int(input("ASCII giriz: "))
+print(asc,"-nyň 'character' bahasy: ")
+print(chr(asc))
